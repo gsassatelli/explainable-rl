94877ad5b73024f7f1883364a178e8e9944dcc6b
mgm21 <mgm18@ic.ac.uk>
2023-01-21 15:13:58 +0000

    Added basic structure of the Engine class

--- foundation/engine.py
+++ foundation/engine.py
@@ -1,1 +1,21 @@
-# Random comment
+import agent
+import environment
+
+class Engine:
+    def __init__(self):
+        pass
+
+    def create_world(self):
+        pass
+
+    def train_agent(self, n_episodes, n_steps):
+        pass
+
+    def get_results(self):
+        pass
+
+    def save_results(self):
+        pass
+
+    def evaluate(self, state):
+        pass
