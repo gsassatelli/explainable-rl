337f7b8b6192e2fa70f04647f26006236c026c53
Giulia Sassatelli <giulia.sassatelli@gmail.com>
2023-02-05 17:40:19 +0000

    First version of engine

--- foundation/engine.py
+++ foundation/engine.py
@@ -9,32 +9,38 @@ from environment import MDP
 
 class Engine:
 
-    __slots__ = ["dataset", "agent_type", "env_type", "agent", "env",
-                 "episode_flag", "num_episodes", "num_steps"]
+    __slots__ = ["dataset", "agent_type", "env_type", "agent", "env", "gamma",
+                 "episode_flag", "num_episodes", "num_steps", "policy", "q_table"]
 
     def __init__(self, 
                  dataset: np.array[np.array, np.array, np.arraynp.array],
                  agent_type: str, 
                  env_type: str,
                  num_episodes: int,
-                 num_steps: int,
-                 episode_flag: bool = False):
+                 num_steps: int):
         """Initialise the Engine superclass.
         Dataset is a multi-type np.array [state, action, reward, next_state].
 
-        TODO: add hyperparameters for training wherever they need to go.
         """
         # Save dataset to train
         self.dataset = dataset
-        # Initialize episode length and steps
+
+        # Hyperparameters
         self.num_episodes = num_episodes
         self.num_steps = num_steps
+        self.gamma = gamma
+
         # Initialize agent
         self.agent_type = agent_type
         self.agent = None
+
         # Initialize environment
         self.env_type = env_type
         self.env = None
+
+        # Parameters of the agent
+        self.policy = None
+        self.q_table = None
 
     def create_world(self):
         """Create the Agent and MDP instances for the given task.
@@ -54,14 +60,13 @@ class Engine:
             self.agent = Agent()
             # Create agent
             self.agent.create_agent()
-            # TODO: change if different name
 
     def create_env(self):
         """Create an env and store it in Engine.
 
         """
         # Initialize environment
-        self.env = MDP(self.dataset)
+        self.env = MDP(self.dataset, self.gamma)
         # Create environment
         self.env.initialise_env()
 
@@ -74,7 +79,7 @@ class Engine:
 
     def what_should_i_do_given_state(self):
         """Implement this."""
-        # TODO Giulia
+        # TODO: Not sure if this is needed
         pass
 
     def get_results(self):
@@ -82,21 +87,26 @@ class Engine:
 
         This could be the average return after convergence.
         """
-        # TODO Giulia
+        # TODO: Not sure if this is needed
         pass
 
     def save_parameters(self):
         """Save the parameters learned during training.
 
         This could be e.g. the q-values, the policy, or any other learned parameters.
+
+        TODO: Not sure this function is needed, can call directly agent
+        TODO: Epsilon greedy policy already contains q-values, remove it?
         """
-        # TODO Giulia
-        pass
+        # Save parameters of the trained agent to predict
+        self.policy = self.agent.policy
+        self.q_table = self.agent.q_table
+
 
     def evaluate(self, state):
         """Evaluate the learned policy at a particular state.
 
         This method returns the action that should be taken given a state.
         """
-        # TODO Giulia
-        pass
+        # TODO: ensure that here output is action with max q values (NO exploration)
+        return self.agent.epsilon_greedy_policy(state)
