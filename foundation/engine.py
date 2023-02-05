2ce4746a0e42bec5fb11bace1844f6648cd3245e
Giulia Sassatelli <giulia.sassatelli@gmail.com>
2023-02-05 17:30:14 +0000

    create_env and train_agent.

--- foundation/engine.py
+++ foundation/engine.py
@@ -9,26 +9,26 @@ from environment import MDP
 
 class Engine:
 
-    __slots__ = ["states", "actions", "rewards", "next_states", "agent_type", "env_type",
-                 "agent", "env"]
+    __slots__ = ["dataset", "agent_type", "env_type", "agent", "env",
+                 "episode_flag", "num_episodes", "num_steps"]
 
     def __init__(self, 
                  dataset: np.array[np.array, np.array, np.arraynp.array],
                  agent_type: str, 
                  env_type: str,
+                 num_episodes: int,
+                 num_steps: int,
                  episode_flag: bool = False):
         """Initialise the Engine superclass.
         Dataset is a multi-type np.array [state, action, reward, next_state].
 
         TODO: add hyperparameters for training wherever they need to go.
         """
-        # Split dataset
-        self.states = dataset[0]
-        self.actions = dataset[1]
-        self.rewards = dataset[2]
-        # If episode is more than one step, save next states
-        if episode_flag:
-            self.next_states = dataset[3]
+        # Save dataset to train
+        self.dataset = dataset
+        # Initialize episode length and steps
+        self.num_episodes = num_episodes
+        self.num_steps = num_steps
         # Initialize agent
         self.agent_type = agent_type
         self.agent = None
@@ -50,22 +50,27 @@ class Engine:
 
         """
         if self.agent_type == "q-learner":
+            # Initialize agent
             self.agent = Agent()
+            # Create agent
+            self.agent.create_agent()
             # TODO: change if different name
-        pass
 
     def create_env(self):
         """Create an env and store it in Engine.
 
         """
-        self.env = MDP(self.states, self.actions, self.rewards)
-        pass
+        # Initialize environment
+        self.env = MDP(self.dataset)
+        # Create environment
+        self.env.initialise_env()
 
-    def train_agent(self, n_episodes, n_steps):
-        """Train the agent for a chosen number of steps and episodes."""
-        # TODO Giulia
-        self.agent.fit(n_episodes, n_steps)
-        pass
+    def train_agent(self):
+        """Train the agent for a chosen number of steps and episodes.
+
+        """
+        # Fit the agent
+        self.agent.fit(self.num_episodes, self.num_episodes)
 
     def what_should_i_do_given_state(self):
         """Implement this."""
