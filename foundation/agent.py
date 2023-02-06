93918d4b22a81899d59ffc08e1f38a1ef9bfb4a7
mireiahernandez <mireiahernandezcaralt@outlook.es>
2023-02-06 12:05:29 +0000

    First version of the agent for MVP (only contains exploration and gamma is not included

--- foundation/agent.py
+++ foundation/agent.py
@@ -1,43 +1,80 @@
 import numpy as np
+import sparse
+import random
 
 class Agent():
-    def __init__(self, state, q_values, policy, env):
+    def __init__(self, env):
         """Initialise the agent superclass."""
         self.env = env
-        self.state = None
-        self.q = ...
+        self.Q = None
+        self.state_to_action = None
+        self.gamma = 0.9
+        self.initialize_agent()
+        #self.state = None
+        #self.q = ...
         # self.rewards_per_episode = []
         # self.total_episode_reward = 0
         pass
 
-    def fit(self, n_episodes, n_steps):
+    def fit(self, n_episodes, n_steps, lr=1):
         """Fit agent to dataset.
 
         TODO: decay. """
-        for _ in n_episodes:
+        
+        
+        for _ in range(n_episodes):
 
-            self.state = self.env.reset()
+            #self.state = self.env.reset()
+            self.state_str = random.choice(list(self.state_to_action.keys()))
+            self.state = [int(s) for s in self.state_str.split(",")]
             for i in range(n_steps):
-                action = self.epsilon_greedy_policy(self.state)
+                action = self.epsilon_greedy_policy(self.state_str)
                 state, next_state, reward, done = self.env.step(self.state,
                                                                 action)
-                self.update_q_values(state, action, next_state, reward)
+
+                self.update_q_values(state, action, next_state, reward, lr)
+
                 self.state = next_state
                 if done:
                     break
 
-        pass
+        breakpoint()
 
-    def update_q_values(self, state, action, next_state, reward):
-        """Update the q table of the agent using Bellman."""
+    def update_q_values(self, state, action, next_state, reward, lr):
+        """Update the q table of the agent using Bellman.
+        
+        TODO: implement part related to gamma (not necessary for now
+        because we have a myopic env)
+        """
+        breakpoint()
+        self.Q[state[0],state[1],state[2],action]  = \
+            (1-lr)*self.Q[state[0],state[1],state[2],action] + lr*(reward)
 
-        pass
-
-    def create_agent(self):
+    def initialize_agent(self):
         """Reset the environment. Called by agent when the episode starts."""
-        pass
+        # reset environment
+        self.env.reset()
+        
+        # create q-table
+        coords = []
+        for state_str, actions in self.env.state_to_action.items():
+            state = [int(s) for s in state_str.split(",")]
+            actions = list(actions)
+            for action in actions:
+                coords.append(state+[action])
+        q_values = np.zeros(len(coords))
+        coords = np.array(coords).T.tolist()
+        
+        self.Q = sparse.COO(coords, q_values)
+        
+        # create state to action mapping
+        self.state_to_action = self.env.state_to_action
+
 
     def epsilon_greedy_policy(self, state):
-        """Returns the epsilon greedy action."""
-        pass
-    
+        """Returns the epsilon greedy action.
+        
+        TODO: implement the "epsilon part"
+        """
+        return random.choice(list(self.state_to_action[state]))
+        
