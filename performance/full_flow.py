b0f4ff058b140626b4b0ea838a339674b2f11ae7
mgm21 <mgm18@ic.ac.uk>
2023-02-21 14:28:19 +0000

    Added a directory and file to test out full memory performance of test run

---
+++ performance/full_flow.py
@@ -0,0 +1,41 @@
+# To view the memory usage without saving to the results, run: python3 -m memory_profiler performance/full_flow.py
+# To save the memory data to a data file, run: mprof run performance/full_flow.py
+
+from foundation.engine import Engine
+from data_handler.data_handler import DataHandler
+from datetime import datetime
+from memory_profiler import profile
+
+@profile
+def evaluate_full_flow_performance():
+    # Load data
+    timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    print(timestamp + ": Load data")
+    states = ['competitorPrice', 'adFlag', 'availability']
+    actions = ['price']
+    rewards = ['revenue']
+    dh = DataHandler('kaggle-dummy-dataset/train.csv', states, actions, rewards)
+
+    # Preprocess the data
+    timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    print(timestamp + ": Preprocess data")
+    dh.prepare_data_for_engine(col_delimiter='|', cols_to_normalise=states + actions)
+
+    # Create engine
+    timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    print(timestamp + ": Initialize Engine")
+    engine = Engine(dh.mdp_data[:1000], "q_learner", "kaggle", num_episodes=100, num_steps=1)
+
+    # Create world
+    timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    print(timestamp + ": Create the world")
+    engine.create_world()
+
+    # Train agent
+    timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    print(timestamp + ": Train the agent on 100 samples")
+    engine.train_agent()
+
+
+if __name__ == "__main__":
+    evaluate_full_flow_performance()
