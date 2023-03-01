c64a64bed8e0001e63bec9189ea6a5e59239ec32
Teresa Delgado de las Heras <trd22@ic.ac.uk>
2023-03-01 10:20:52 +0100

    Final changes before merging to dev

--- foundation/environment.py
+++ foundation/environment.py
@@ -5,51 +5,63 @@ import numpy as np
 class MDP:
     """Defines and instantiates an MDP object.
     """
-    __slots__ = ["mdp_data", "average_rewards", "num_bins", "state_to_action", "bins_dict", "ix", "state_mdp_data", "action_mdp_data", "reward_mdp_data"]
+    __slots__ = ["dh", "_average_rewards", "_num_bins", "_state_to_action", "bins_dict", "ix", "_state_mdp_data", "_action_mdp_data", "_reward_mdp_data"]
 
-    def __init__(self, mdp_data):
+    def __init__(self, dh):
         """Initialises the MDP superclass.
         Args:
-            mdp_data (pd.DataFrame): Dataframe containing states, actions and rewards.
+            dh (DataHandler): Data handler object.
         """
-        self.average_rewards = None
-        self.state_to_action = {}
-        self.mdp_data = mdp_data
-        self.state_mdp_data = None
-        self.action_mdp_data = None
-        self.reward_mdp_data = None
-        self.num_bins = 100
+        self._average_rewards = None
+        self._state_to_action = {}
+        self.dh = dh
+
+        self._state_mdp_data = None
+        self._action_mdp_data = None
+        self._reward_mdp_data = None
+        self._num_bins = 100
+
+
         self.initialise_env()
         
 
     def initialise_env(self):
         """Creates the environment given the MDP information."""
-        self.average_rewards = self.make_rewards_from_data()
+        self._average_rewards = self._make_rewards_from_data()
 
-    def join_state_action(self):
+    def _transform_df_to_numpy(self):
+        """Transforms the MDP data from a dataframe to a numpy array
+        """
+        self._state_mdp_data = self.dh.get_states.to_numpy()
+        self._action_mdp_data = self.dh.get_actions.to_numpy()
+        self._reward_mdp_data = self.dh.get_rewards.to_numpy()
+
+
+    def _join_state_action(self):
         """Joins the state and action pairs together.
         Returns:
             list: Group of states and actions per datapoint.
         """
        
         zipped = []
-        for i in range(len(self.mdp_data)):
-            state_array = self.mdp_data["s"].iloc[i].tolist()
-            action_array =  self.mdp_data["a"].iloc[i].tolist()
+        for i in range(len(self._reward_mdp_data)):
+            state_array = self._state_mdp_data[i].tolist()
+            action_array =  self._action_mdp_data[i].tolist()
             zipped.append(state_array + action_array)
         return zipped
 
-    def bin_state_action_space(self, zipped):
+    def _bin_state_action_space(self, zipped):
         """Bins the state-action pairs.
         Args:
             zipped (list): Group of states and actions per datapoint.
         Returns:
             np.array: Binned state-action pairs.
         """
-        self.bins = np.arange(0, 1 + 1/self.num_bins, step=1/self.num_bins).tolist()
+        self.bins = np.arange(0, 1 + 1/self._num_bins, step=1/self._num_bins).tolist()
         return np.digitize(zipped, self.bins, right=True)
 
-    def get_counts_and_rewards_per_bin(self, binned):
+
+    def _get_counts_and_rewards_per_bin(self, binned):
         """Creates a dictionary of counts of datapoints per bin and sums the associated rewards. 
         Args:
             binned (np.array): Binned state-action pairs.
@@ -57,22 +69,20 @@ class MDP:
             dict: dictionary of counts of datapoints per bin and sums the associated rewards.
         """
         bins_dict = {}
-        self.state_to_action = {}
+        self._state_to_action = {}
         for ix, bin in enumerate(binned):
-            reward = self.mdp_data["r"].iloc[ix]
-            # update state to action
-            state = ",".join([str(s) for s in bin[:-1]])
-            self.state_to_action.setdefault(state, set()).add(bin[-1])
-            
-            # update bin_dict
-            bin = str(bin).replace("[", "").replace("]", "")
+            bin = ",".join(str(e) for e in bin.tolist())
+            # Update state to action
+            self._state_to_action.setdefault(bin[:-2], set()).add(int(bin[-1]))
+
+            # Update bins_dict
             bins_dict[bin][0] = bins_dict.setdefault(bin, [0, 0])[0] + 1
-            reward = self.reward_mdp_data[ix]
+            reward = self._reward_mdp_data[ix]
             bins_dict[bin][1] += reward[0]
         return bins_dict
     
 
-    def create_average_reward_matrix(self, bins_dict):
+    def _create_average_reward_matrix(self, bins_dict):
         """Generates a sparse matrix of average rewards for each bin in the dataset.
         Args:
             bins_dict (dict): dictionary of counts of datapoints per bin and sums the associated rewards.
@@ -90,7 +100,8 @@ class MDP:
         coords = np.array(coords).T.tolist()
         return sparse.COO(coords, data)
 
-    def make_rewards_from_data(self):
+
+    def _make_rewards_from_data(self):
         """Creates sparse matrix of the state-action pairs and associated rewards from the inputted dataset.
         Returns:
             sparse.COO: sparse matrix of binned state-action pairs and their associate average reward.
@@ -98,16 +109,16 @@ class MDP:
         print("Create average rewards matrix")
 
         # Transform data for efficiency
-        self.transform_df_to_numpy()
+        self._transform_df_to_numpy()
 
-        zipped = self.join_state_action()
+        zipped = self._join_state_action()
 
         # Create the bins
-        binned = self.bin_state_action_space(zipped)
+        binned = self._bin_state_action_space(zipped)
        
-        bins_dict = self.get_counts_and_rewards_per_bin(binned)
+        bins_dict = self._get_counts_and_rewards_per_bin(binned)
 
-        average_reward_matrix = self.create_average_reward_matrix(bins_dict)
+        average_reward_matrix = self._create_average_reward_matrix(bins_dict)
 
         return average_reward_matrix
   
@@ -116,9 +127,9 @@ class MDP:
         Returns:
             list: Randomised initial state.
         """
-        sample_ix_point = np.random.choice(np.arange(len(self.state_mdp_data) + 1))
-        state = self.state_mdp_data[sample_ix_point].tolist()
-        binned_state = self.bin_state_action_space(state)
+        sample_ix_point = np.random.choice(np.arange(len(self._state_mdp_data) + 1))
+        state = self._state_mdp_data[sample_ix_point].tolist()
+        binned_state = self._bin_state_action_space(state)
         return binned_state[0]
 
     def step(self, state, action):
@@ -131,6 +142,6 @@ class MDP:
         Returns:
             tuple: current state, action, next state, done flag.
         """
-        reward = self.average_rewards[state[0],state[1],state[2],action]
+        reward = self._average_rewards[state[0],state[1],state[2],action]
 
         return state, state, reward, True
