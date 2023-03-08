f0c1b6e418537a6d4cff4d01ad157700eebea48b
mgm21 <mgm18@ic.ac.uk>
2023-03-08 10:16:06 +0000

    Added to the Agent super class

--- src/foundation/super_classes.py
+++ src/foundation/super_classes.py
@@ -1,12 +1,31 @@
 class Agent:
+    """Define the Agent super class which all particular agents (e.g Q-learner, SARSA).
+    """
     __slots__ = ['env', 'gamma']
 
     def __init__(self, env, gamma):
         self.env = env
         self.gamma = gamma
 
+    def fit(self, n_episodes, n_steps, lr=0.1, lr_decay=0.05, lr_min=0.01,
+            epsilon=0.1, epsilon_decay=0.05, epsilon_min=0.01, verbose=False):
+        """Fit agent to the dataset.
+
+        Args:
+            n_episodes (int): number of episodes.
+            n_steps (int): number of steps per episode.
+            lr (float): learning rate.
+            lr_decay (float): learning rate decay.
+            lr_min (float): minimum learning rate.
+            epsilon (float): epsilon-greedy policy parameter.
+            epsilon_decay (float): epsilon decay.
+            epsilon_min (float): minimum epsilon.
+            verbose (bool): print training information.
+        """
+        raise NotImplementedError
+
 class MDP:
-    """Define the MDP super class which all particular MDP should inherit from
+    """Define the MDP super class which all particular MDP should inherit from.
     """
     __slots__ = ['dh']
     def __init__(self, dh):
