768711e1a6e8f84fa75b8a867bcc89f7f9808a01
Giulia Sassatelli <giulia.sassatelli@gmail.com>
2023-04-13 12:28:38 +0200

    First functions.

--- src/explainability/shap_values.py
+++ src/explainability/shap_values.py
@@ -2,22 +2,47 @@ class ShapValues:
     """ This class implements the SHAP values algorithm.
     """
 
-    __slots__ = []
+    __slots__ = ["sample", "features"]
 
-    def __init__(self):
-        pass
+    def __init__(self,
+                 sample,
+                 features):
+        self.sample = sample
+        self.features = features
 
-    # TODO:
-    # - Get sample from user
-    # - Find cell for that sample >> What if the sample is not in the dataset?
+    def compute_shape_values(self):
+
+        if not self.verify_sample_length():
+            raise ValueError("The sample length is not correct.")
+
+    # - Find cell for that sample
+    # - Verify if cell has been visited
     # - Loop over all state dimensions:
     #   - Loop for n times
-    #       - Sample with dimension fixed until sample is in the dataset
+    #       - Sample with dimension fixed until sample has been visited
     #       - Get action for that sample
-    #       - Sample randomly until sample is in the dataset
+    #       - Sample randomly until sample has been visited
     #       - Get action for that sample
     #   - Calculate difference
     #   - Calculate average difference
     #   - Output q-value for that state dimension
 
-    # Start code
+        pass
+
+    def verify_sample_length(self):
+        """ This function verifies if the sample length is correct.
+        """
+        if len(self.sample) != len(self.features):
+            return False
+        return True
+
+    def bin_sample(self):
+        """ This function bins the sample.
+        """
+        pass
+
+    def verify_cell_availability(self):
+        """ This function verifies if the cell has been visited.
+        """
+        pass
+
