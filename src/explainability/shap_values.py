a626e3d356486aab204fb97b68d19dfe2998bbfb
gsassatelli <115473215+gsassatelli@users.noreply.github.com>
2023-04-13 11:38:51 +0200

    Create shap_values.py

---
+++ src/explainability/shap_values.py
@@ -0,0 +1,21 @@
+class ShapValues:
+    """ This class implements the SHAP values algorithm.
+    """
+
+    __slots__ = []
+
+    def __init__(self):
+        pass
+
+    # TODO:
+    # - Get sample from user
+    # - Find cell for that sample >> What if the sample is not in the dataset?
+    # - Loop over all state dimensions:
+    #   - Loop for n times
+    #       - Sample with dimension fixed until sample is in the dataset
+    #       - Get action for that sample
+    #       - Sample randomly until sample is in the dataset
+    #       - Get action for that sample
+    #   - Calculate difference
+    #   - Calculate average difference
+    #   - Output q-value for that state dimension
