52b1a3cf6be3b4552cb3d8d36be769fc67e85a2d
mgm21 <mgm18@ic.ac.uk>
2023-04-16 10:53:13 +0100

    Performance evaluator calling outside function for run loop

--- src/performance/performance_evaluator.py
+++ src/performance/performance_evaluator.py
@@ -9,6 +9,8 @@ from src.foundation.engine import Engine
 from src.data_handler.data_handler import DataHandler
 from src.explainability.pdp import PDP
 from datetime import datetime
+from main import run_all
+
 
 class PerformanceEvaluator:
     def __init__(self):
@@ -246,70 +248,13 @@ class PerformanceEvaluator:
             num_samples (int): Number of datapoint samples used for training.
 
         """
-        # TODO: call the outside function.
         if self.verbose:
             print(f"-> Running training loop for {num_episodes} episodes, {num_bins} bins, {num_samples} samples")
 
         # Get the hyperparameter dictionary for the specified parameters
         hyperparam_dict = self.get_hyperparam_dict_ds_data(num_episodes, num_bins, num_samples)
 
-        timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        #print(f"{timestamp}: Load data")
-        states = hyperparam_dict['states']
-        actions = hyperparam_dict['actions']
-        rewards = hyperparam_dict['rewards']
-        n_samples = hyperparam_dict['n_samples']
-        dh = DataHandler(data_path=hyperparam_dict['data_path'],
-                         state_labels=states,
-                         action_labels=actions,
-                         reward_labels=rewards,
-                         n_samples=n_samples)
-
-        # Preprocess the data
-        timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        #print(f"{timestamp}: Preprocess data")
-        dh.prepare_data_for_engine(col_delimiter=hyperparam_dict['col_delimiter'],
-                                   cols_to_normalise=hyperparam_dict[
-                                       'cols_to_normalise'])
-
-        # Create engine
-        timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-
-        #print(f"{timestamp}: Initialize Engine")
-        engine = Engine(dh,
-                        agent_type=hyperparam_dict['agent_type'],
-                        env_type=hyperparam_dict['env_type'],
-                        num_episodes=hyperparam_dict['num_episodes'],
-                        num_steps=hyperparam_dict['num_steps'],
-                        bins=hyperparam_dict['bins']
-                        )
-        # Create world
-        timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        #print(f"{timestamp}: Create the world")
-        engine.create_world()
-
-        # Train agent
-        timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        #print(f"{timestamp}: Train the agent on {n_samples} samples")
-        engine.train_agent()
-
-        # TODO: removed PDP plotting for now because having many plots successively hinders flow of performance check
-        #  this should not be a problem considering the build_data_for_plots has been kept (it is space/time consuming)
-        # Plot PDPs
-        timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        #print(f"{timestamp}: Show PDPs plots")
-        pdp = PDP(bins=engine.env.bins,
-                  minmax_scalars=dh.minmax_scalars,
-                  action_labels=actions,
-                  state_labels=states)
-        pdp.build_data_for_plots(engine.agent.Q, engine.agent.Q_num_samples)
-        type_features = hyperparam_dict['feature_types']
-        # fig_name = "PDP plots - All states"
-        # pdp.plot_pdp(states_names=states, fig_name=fig_name,
-        #              type_features=type_features, savefig=True, all_states=True)
-        # fig_name = "PDP plots - Visited states"
-        # pdp.plot_pdp(states_names=states, fig_name=fig_name,
-        #              type_features=type_features, savefig=True, all_states=False)
+        run_all(hyperparam_dict)
 
     def get_hyperparam_dict_ds_data(self, num_episodes, num_bins, num_samples):
         """Load the hyperparameter dictionaries for the Datasparq data.
@@ -353,10 +298,6 @@ class PerformanceEvaluator:
 if __name__ == "__main__":
     performance_evaluator = PerformanceEvaluator()
     performance_evaluator.get_all_performance_evaluations()
-
-
-# TODO: now that all the content has been obtained, use html to create pdf from results:
-#  https://towardsdatascience.com/how-to-easily-create-a-pdf-file-with-python-in-3-steps-a70faaf5bed5
 
 # TODO: remove all the print statements from the other classes (environment, agent, etc...) because it clouds the output
 #  here.
