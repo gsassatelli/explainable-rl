a55e58a82b9455f7a25f511a19c811fede91afae
Giulia Sassatelli <giulia.sassatelli@gmail.com>
2023-04-18 15:28:50 +0100

    Refactor test_data_handler.py

--- tests/test_data_handler/test_data_handler.py
+++ tests/test_data_handler/test_data_handler.py
@@ -1,48 +1,54 @@
-# import ipdb 
-import numpy as np
 from src.data_handler.data_handler import DataHandler
-import unittest 
+import unittest
 import pandas as pd
 
-# Load data
-states = ['competitorPrice', 'adFlag', 'availability']
-actions = ['price']
-rewards = ['revenue']
-n_samples = 2000
-dh = DataHandler('tests/test_env_data.csv', states, actions, rewards, n_samples=n_samples)
-dh.prepare_data_for_engine(col_delimiter=',', cols_to_normalise=states+actions)
 
-def test_load_data():
-    dh.load_data(delimiter=',')
-    assert type(dh.dataset) == pd.core.frame.DataFrame
+class TestDataHandler(unittest.TestCase):
+    dh = None
 
-def test_type_get_actions():
-    result = dh.get_actions()
-    assert isinstance(result, list)
+    @classmethod
+    def setUpClass(cls) -> None:
+        states = ['competitorPrice', 'adFlag', 'availability']
+        actions = ['price']
+        rewards = ['revenue']
+        n_samples = 200
+        cls.dh = DataHandler('tests/test_env_data.csv', states, actions, rewards, n_samples=n_samples)
+        cls.dh.prepare_data_for_engine(col_delimiter=',', cols_to_normalise=states + actions)
 
-def test_len_get_actions():
-    result = dh.get_actions()
-    assert len(result) != 0
+    def setUp(self) -> None:
+        pass
 
-def test_type_get_rewards():
-    result = dh.get_rewards()
-    assert type(result) == pd.core.frame.DataFrame
+    def tearDown(self) -> None:
+        pass
 
-def test_len_get_rewards():
-    result = dh.get_rewards()
-    assert len(result) != 0
+    def test_load_data(self):
+        self.dh.load_data(delimiter=',')
+        assert isinstance(self.dh.dataset, pd.DataFrame)
 
-def test_type_get_states():
-    result = dh.get_states()
-    assert type(result) == pd.core.frame.DataFrame
+    def test_type_get_actions(self):
+        result = self.dh.get_actions()
+        assert isinstance(result, list)
 
-def test_len_get_states():
-    result = dh.get_states()
-    assert len(result) != 0
+    def test_len_get_actions(self):
+        result = self.dh.get_actions()
+        assert len(result) != 0
 
-def test_filter_data():
-    dh._filter_data()
-    assert dh.dataset.isnull().values.any() == False
+    def test_type_get_rewards(self):
+        result = self.dh.get_rewards()
+        assert isinstance(result, pd.DataFrame)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_len_get_rewards(self):
+        result = self.dh.get_rewards()
+        assert len(result) != 0
+
+    def test_type_get_states(self):
+        result = self.dh.get_states()
+        assert isinstance(result, pd.DataFrame)
+
+    def test_len_get_states(self):
+        result = self.dh.get_states()
+        assert len(result) != 0
+
+    def test_filter_data(self):
+        self.dh._filter_data()
+        assert self.dh.dataset.isnull().values.any() == False
