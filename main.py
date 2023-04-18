f064f516cb9e130dc11fb1c5e7e5e7292273bdeb
mireiahernandez <mireiahernandezcaralt@outlook.es>
2023-04-18 09:05:10 +0200

    Main now incorporates evaluation pipeline

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ def policy_deviation():
 def run_all(hyperparam_dict):
     timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     print(f"{timestamp}: Load data")
-    states = hyperparam_dict['states']
-    actions = hyperparam_dict['actions']
-    rewards = hyperparam_dict['rewards']
+    state_labels = hyperparam_dict['states']
+    action_labels = hyperparam_dict['actions']
+    reward_labels = hyperparam_dict['rewards']
     n_samples = hyperparam_dict['n_samples']
     dh = DataHandler(data_path=hyperparam_dict['data_path'],
-                     state_labels=states,
-                     action_labels=actions,
-                     reward_labels=rewards,
+                     state_labels=state_labels,
+                     action_labels=action_labels,
+                     reward_labels=reward_labels,
                      n_samples=n_samples)
 
     # Preprocess the data
@@ -55,7 +55,7 @@ def run_all(hyperparam_dict):
 
     # TODO: denorm states, actions and rewards (using datahandler's inverse scaling)
 
-    states, actions, rewards_hist, actions_agent, rewards_agent = \
+    states, actions, b_actions, rewards_hist, actions_agent, b_actions_agent, rewards_agent = \
         engine.evaluate_agent()
     # Sum obtained reward optimal vs historical policy
     import numpy as np
@@ -66,7 +66,6 @@ def run_all(hyperparam_dict):
     plt.scatter(actions, actions_agent)
     plt.savefig('policy.png')
 
-    ipdb.set_trace()
 
     ###########################################################
     ################# End of evaluation #######################
@@ -77,15 +76,15 @@ def run_all(hyperparam_dict):
     print(f"{timestamp}: Show PDPs plots")
     pdp = PDP(bins=engine.env.bins,
               minmax_scalars=dh.minmax_scalars,
-              action_labels=actions,
-              state_labels=states)
+              action_labels=action_labels,
+              state_labels=state_labels)
     pdp.build_data_for_plots(engine.agent.Q, engine.agent.Q_num_samples)
     type_features = hyperparam_dict['feature_types']
     fig_name = "PDP plots - All states"
-    pdp.plot_pdp(states_names=states, fig_name=fig_name,
+    pdp.plot_pdp(states_names=state_labels, fig_name=fig_name,
                  type_features=type_features, savefig=True, all_states=True)
     fig_name = "PDP plots - Visited states"
-    pdp.plot_pdp(states_names=states, fig_name=fig_name,
+    pdp.plot_pdp(states_names=state_labels, fig_name=fig_name,
                  type_features=type_features, savefig=True, all_states=False)
 
 
@@ -104,14 +103,14 @@ if __name__ == "__main__":
             'price': "continuous",
             'reward': "continuous"
         },
-        'n_samples': 10,
+        'n_samples': 1000,
         'data_path': 'data/ds-data/my_example_data.parquet',
         'col_delimiter': '|',
         'cols_to_normalise': ['lead_time', 'length_of_stay',
                    'competitor_price_difference_bin', 'demand_bin', 'price', 'reward'],
         'agent_type': 'q_learner',
         'env_type': 'strategic_pricing',
-        'num_episodes': 10,
+        'num_episodes': 1000,
         'num_steps': 1,
         'train_test_split': 0.2
     }
