7a5ddbe38414d0f7506fc5e391b087cce61e5aac
Giulia Sassatelli <giulia.sassatelli@gmail.com>
2023-04-19 12:41:48 +0100

    Tests for pdp e shap

--- tests/test_explainability/test_shap_values.py
+++ tests/test_explainability/test_shap_values.py
@@ -1,2 +1,102 @@
-def test_shap_values():
-    pass
+import unittest
+from src.explainability.shap_values import ShapValues
+from src.foundation.engine import Engine
+from src.data_handler.data_handler import DataHandler
+
+
+class TestPDP(unittest.TestCase):
+    """Test PDP class.
+    """
+
+    dh = None
+    shap_values = None
+    engine = None
+
+    @classmethod
+    def setUpClass(cls):
+        """Setup PDP class.
+        """
+        states = ['competitorPrice', 'adFlag', 'availability']
+        actions = ['price']
+        rewards = ['revenue']
+        n_samples = 50
+        cls.dh = DataHandler('tests/test_env_data.csv', states, actions, rewards, n_samples=n_samples)
+        cls.dh.prepare_data_for_engine(col_delimiter=',', cols_to_normalise=states + actions)
+        cls.engine = Engine(cls.dh,
+                            agent_type="q_learner",
+                            env_type="strategic_pricing",
+                            bins=[10, 10, 10, 10],
+                            num_episodes=100,
+                            num_steps=1)
+        cls.engine.create_world()
+        cls.engine.train_agent()
+        cls.shap_values = ShapValues(sample=[8, 0.5, 1], features=states, env=cls.engine.env,
+                                     Q=cls.engine.agent.Q, minmax_scalars=cls.dh.minmax_scalars, action=actions,
+                                     number_of_samples=10)
+
+    def test_create_shap_values(self):
+        """Test creation of ShapValues object.
+        """
+        assert isinstance(self.shap_values, ShapValues)
+
+    def test_compute_shap_values(self):
+        """Test compute_shap_values method.
+        """
+        shaps, predicted_action = self.shap_values.compute_shap_values()
+
+        assert isinstance(shaps, list)
+        assert isinstance(predicted_action, float)
+        assert len(shaps) == len(self.shap_values.features)
+
+    def test_verify_sample_length(self):
+        """Test verify_sample_length method.
+        """
+        result = self.shap_values.verify_sample_length()
+        assert isinstance(result, bool)
+
+    def test_bin_sample(self):
+        """Test bin_sample method.
+        """
+        result = self.shap_values.bin_sample()
+        assert isinstance(result, list)
+        assert len(result) == len(self.shap_values.sample)
+
+    def test_verify_cell_availability(self):
+        """Test verify_cell_availability method.
+        """
+        binned_sample = [0, 0, 0]
+        result = self.shap_values.verify_cell_availability(binned_sample)
+        assert isinstance(result, bool)
+
+    def test_sample_plus_minus_samples(self):
+        """Test sample_plus_minus_samples method.
+        """
+        shap_ft = 0
+        num_bins_per_shap_ft = 10
+        result_plus, result_minus = self.shap_values.sample_plus_minus_samples(shap_ft, num_bins_per_shap_ft)
+        assert isinstance(result_plus, list)
+        assert isinstance(result_minus, list)
+        assert len(result_plus) == len(result_minus)
+        assert len(result_plus) == len(self.shap_values.sample)
+
+    def test_get_denorm_actions(self):
+        """Test get_denorm_actions method.
+        """
+        actions = [0, 0, 0, 0, 0]
+        result = self.shap_values.get_denorm_actions(actions)
+        assert isinstance(result, list)
+        assert len(result) == len(actions)
+
+    def test_normalize_sample(self):
+        """Test normalize_sample method.
+        """
+        result = self.shap_values.normalize_sample()
+        assert isinstance(result, list)
+        assert len(result) == len(self.shap_values.sample)
+
+    def test_predict_action(self):
+        """Test predict_action method.
+        """
+        result = self.shap_values.predict_action()
+        assert isinstance(result, float)
+
