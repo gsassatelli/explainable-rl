a1902bd72a740096d1da181a127bbdd9d1a5bd1a
Teresa Delgado de las Heras <trd22@ic.ac.uk>
2023-04-19 12:37:02 +0100

    Added new hyperparameter dictionaries for different environments

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def run_all(hyperparam_dict, verbose=True, show_plots=True):
 
 
 if __name__ == "__main__":
-    hyperparam_dict_ds_data = {
+    hyperparam_dict_ds_data_suggest = {
         'states': ['lead_time', 'length_of_stay',
                    'competitor_price_difference_bin', 'demand_bin', 'price'],
         'actions': [price_bin/10 for price_bin in range(1,11)],
@@ -135,9 +135,9 @@ if __name__ == "__main__":
         'shap_num_samples': 1
     }
 
-    hyperparam_dict_kaggle_data = {
-        'states': ['competitorPrice', 'adFlag', 'availability'],
-        'actions': ['price'],
+    hyperparam_dict_kaggle_data_suggest = {
+        'states': ['competitorPrice', 'adFlag', 'availability', 'price'],
+        'actions': [price_bin/10 for price_bin in range(1,11)],
         'rewards': ['revenue'],
         'feature_types': {
             'competitorPrice': "continuous",
@@ -158,6 +158,59 @@ if __name__ == "__main__":
         'train_test_split': 0.2,
         'shap_num_samples': 1
     }
+
+    hyperparam_dict_ds_data_predict = {
+        'states': ['lead_time', 'length_of_stay',
+                   'competitor_price_difference_bin', 'demand_bin'],
+        'actions': ['price'],
+        # TODO: these correspond to the states and actions. Probably should change to a dict.
+        'rewards': ['reward'],
+        'bins': [10, 10, 4, 4, 10],
+        'feature_types': {
+            'lead_time': "continuous",
+            'length_of_stay': "continuous",
+            'competitor_price_difference_bin': "discrete",
+            'demand_bin': "discrete",
+            'price': "continuous",
+            'reward': "continuous"
+        },
+        'n_samples': 5000,
+        'data_path': 'data/ds-data/my_example_data.parquet',
+        'col_delimiter': '|',
+        'cols_to_normalise': ['lead_time', 'length_of_stay',
+                              'competitor_price_difference_bin', 'demand_bin', 'price', 'reward'],
+        'agent_type': 'q_learner',
+        'env_type': 'strategic_pricing',
+        'num_episodes': 500,
+        'num_steps': 1,
+        'train_test_split': 0.2,
+        'shap_num_samples': 1
+    }
+
+    hyperparam_dict_kaggle_data_predict = {
+        'states': ['competitorPrice', 'adFlag', 'availability'],
+        'actions': ['price'],
+        'rewards': ['revenue'],
+        'feature_types': {
+            'competitorPrice': "continuous",
+            'adFlag': "discrete",
+            'availability': "discrete",
+            'price': "continuous",
+            'revenue': "continuous"
+        },
+        'bins': [10, 2, 2, 10],  # TODO: these correspond to the states and actions. Probably should change to a dict.
+        'n_samples': 20000,
+        'data_path': 'data/kaggle-dummy-dataset/train.csv',
+        'col_delimiter': '|',
+        'cols_to_normalise': ['competitorPrice', 'adFlag', 'availability', 'price'],
+        'agent_type': 'q_learner',
+        'env_type': 'strategic_pricing',
+        'num_episodes': 1000,
+        'num_steps': 1,
+        'train_test_split': 0.2,
+        'shap_num_samples': 1
+    }
+
     for i in range(1):
-        run_all(hyperparam_dict_ds_data)
+        run_all(hyperparam_dict_kaggle_data_predict)
         # ran this 10 times to check everything was fine.
