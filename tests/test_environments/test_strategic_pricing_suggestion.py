481905e6dc8d8f9b0f9be0de8be1c4b23b249c3f
Teresa Delgado de las Heras <trd22@ic.ac.uk>
2023-04-21 11:43:32 +0100

    More tests for new environment

--- tests/test_environments/test_strategic_pricing_suggestion.py
+++ tests/test_environments/test_strategic_pricing_suggestion.py
@@ -25,4 +25,41 @@ class TestStrategicPricingSuggestionMDP(TestStrategicPricing):
         assert isinstance(result, dict)
         assert result == target
 
+    def test_make_rewards_from_data(self):
+        pass
+
+    def test_step(self):
+        pass
+    
+    def test_find_next_state(self):
+        state = [1, 2, 3]
+        action = 2
+        result = self.env._find_next_state(state, action)
+        assert len(result) == 2
+        assert result[0] == [1, 2, 3]
+        assert result[1] == True
+
+    def test_create_average_reward_matrix(self):
+        """Test create_average_reward_matrix method."""
+        counts_rewards = {'1,2,3': [2, 2], '1,2,3': [2, 4]}
+        result = self.env._create_average_reward_matrix(counts_rewards)
+        target = np.zeros((10, 10, 10))
+        target[1, 2, 3] = 1.0
+        target[1, 2, 3] = 2.0
+        target = sparse.COO(target)
+        assert result == target
+
+    def test_transform_df_to_numpy(self):
+        """Test transform_df_to_numpy method."""
+        target_states = self.dh.get_states().to_numpy()
+        target_actions = np.array(self.dh.get_action_labels())
+        target_rewards = self.dh.get_rewards().to_numpy()
+        self.env._transform_df_to_numpy()
+        result_states = self.env._state_mdp_data
+        result_actions = self.env._action_mdp_data
+        result_reward = self.env._reward_mdp_data
+        assert np.array_equal(target_states, result_states)
+        assert np.array_equal(target_actions, result_actions)
+        assert np.array_equal(target_rewards, result_reward)
+
         
