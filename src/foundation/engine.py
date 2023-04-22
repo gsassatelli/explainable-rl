4567c78be1a02abdd2c5ca857176034a2ce0e830
mireiahernandez <mireiahernandezcaralt@outlook.es>
2023-04-22 12:11:05 +0100

    removed evaluation paramaters and methods form engine (except the evaluation methods during training)

--- src/foundation/engine.py
+++ src/foundation/engine.py
@@ -7,7 +7,7 @@ from src.agents.sarsa_lambda import SarsaLambdaAgent
 from src.agents.double_q_learner import DoubleQLearner
 from src.environments.strategic_pricing_suggestion import StrategicPricingSuggestionMDP
 from src.environments.strategic_pricing_prediction import StrategicPricingPredictionMDP
-
+from tqdm import tqdm
 
 class Engine:
     """Responsible for creating the agent and environment instances and running the training loop."""
@@ -43,27 +43,19 @@ class Engine:
         self.policy = None
         self.q_table = None
 
-        # Parameters for evaluation
-        self.agent_cumrewards = None
-        self.hist_cumrewards = None
-        self._eval_states = None
-        self._eval_b_states = None
-        self._eval_actions = None
-        self._eval_rewards = None
-        self._eval_state_dims = None
-        self._eval_action_dims = None
-
-        self._verbose = verbose
+        # Parameters for evaluation        
+        self.evaluate = hyperparam_dict['training']['evaluate']
+        self.num_eval_steps =hyperparam_dict['training']['num_eval_steps']
 
     def create_world(self):
         """Create the Agent and MDP instances for the given task."""
         # Create chosen environment
-        if self._verbose:
+        if self.verbose:
             print("Initialize environment")
         self.create_env()
         
         # Create chosen agent
-        if self._verbose:
+        if self.verbose:
             print("Initialize agent")
         self.create_agent()
 
@@ -108,9 +100,7 @@ class Engine:
         else:
             raise NotImplementedError
 
-    def train_agent(self,
-                    evaluate=False,
-                    n_eval_steps=10):
+    def train_agent(self):
         """Train the agent for a chosen number of steps and episodes.
 
         Args:
@@ -118,19 +108,23 @@ class Engine:
             n_eval_steps (int): Number of evaluation steps.
         """
         # Fit the agent
-        if not evaluate:
+        if not self.evaluate:
             self.agent.fit(agent_hyperparams=self.hyperparameters['agent'],
                            training_hyperparams=self.hyperparameters['training'],
                            verbose=self.verbose)
         
         self.agent_cumrewards = []
-        if evaluate:
+        if self.evaluate:
             self.build_evaluation()
-            for i in range(int(self.num_episodes/n_eval_steps)):
-                self.agent.fit(self.hyperparameters['agent'],
-                               verbose=self.verbose)
-                self.agent_cumrewards.append(self._evaluate_total_agent_reward())
-            self.hist_cumrewards = self._evaluate_total_hist_reward()
+            self.hyperparameters['training']['num_episodes'] = self.num_eval_steps
+            with tqdm(total=self.num_episodes) as pbar:
+                for i in range(int(self.num_episodes/self.num_eval_steps)):
+                    self.agent.fit(agent_hyperparams=self.hyperparameters['agent'],
+                            training_hyperparams=self.hyperparameters['training'],
+                            verbose=self.verbose,
+                            pbar=pbar)
+                    self.agent_cumrewards.append(self._evaluate_total_agent_reward())
+                self.hist_cumrewards = self._evaluate_total_hist_reward()
 
     def _inverse_scale_feature(self,
                                values,
@@ -253,74 +247,6 @@ class Engine:
                                                     self.dh.reward_labels)
 
         return np.sum(rewards_hist)
-
-    def evaluate_agent(self, epsilon=0):
-        """Evaluate the learned policy for the test states.
-        Rewards are calculated using the average reward matrix.
-
-        Args:
-            epsilon: value of epsilon in the epsilon-greedy policy
-                (default= 0 corresponds to pure exploitation)
-        Returns:
-            states (list): list of test states
-            actions (list): list of test actions (historical)
-            rewards_hist (list): list of historical rewards (calculated)
-            actions_agent (list): list of recommended actions
-            rewards_agent (list): list of rewards obtained by agent (calculated)
-        """
-        # Define dictionary containing evaluation results
-        eval_results = {}
-
-        # Save training results
-        eval_results['agent_cumrewards'] = self.agent_cumrewards
-        eval_results['hist_cumrewards'] = self.hist_cumrewards
- 
-        # Get test data from data handler
-        states = self.dh.get_states(split='test').to_numpy().tolist()
-        actions = self.dh.get_actions(split='test').to_numpy().tolist()
-        rewards = self.dh.get_rewards(split='test').to_numpy().tolist()
-
-        # get state and action indexes
-        state_dims = list(range(self.env.state_dim))
-        action_dims = list(range(self.env.state_dim, 
-                                 self.env.state_dim+self.env.action_dim))
-        # Get the binned states
-        b_states = self.env.bin_states(states, idxs=state_dims)
-        # Inverse scaling
-        states = self._inverse_scale_feature(states, self.dh.state_labels)
-
-        # Get the binned actions
-        b_actions = self.env.bin_states(actions, idxs=action_dims)
-
-        # Get actions corresponding to agent's learned policy
-        b_actions_agent = self.agent.predict_actions(b_states)
-
-        # De-bin the recommended actions
-        actions_agent = self.env.debin_states(b_actions_agent, idxs=action_dims)
-
-        # Get reward based on agent policy
-        rewards_agent = self.agent.predict_rewards(b_states, b_actions_agent)
-
-        # Get reward based on historical policy
-        rewards_hist = self.agent.predict_rewards(b_states, b_actions)
-
-        #  Apply inverse scaling to actions, states, and rewards
-        eval_results['states'] = self._inverse_scale_feature(states,
-                                            self.dh.state_labels)
-        eval_results['actions_hist'] = self._inverse_scale_feature(actions,
-                                                    self.dh.action_labels)
-        eval_results['actions_agent'] = self._inverse_scale_feature(actions_agent,
-                                                    self.dh.action_labels)
-        eval_results['rewards_hist'] = self._inverse_scale_feature(rewards_hist,
-                                                    self.dh.reward_labels)
-        eval_results['rewards_agent'] = self._inverse_scale_feature(rewards_agent,
-                                                    self.dh.reward_labels)
-        
-        # Save additional arrays
-        eval_results['b_actions'] = b_actions
-        eval_results['b_actions_agent'] = b_actions_agent
-        
-        return eval_results
 
     def _get_bins(self):
         """Get the bins for the states and actions.
